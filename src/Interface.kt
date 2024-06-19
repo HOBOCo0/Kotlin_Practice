e7aa1716b89aaeb190fe7144445270a9c7ec4223
aman1 <aman15220@gmail.com>
2024-06-19 13:36:42 +0530

    kot_Interfaces_Exp

---
+++ src/Interface.kt
@@ -0,0 +1,16 @@
+/*
+Interface
+ --> An interface is a collection of abstract methods and properties that defines a common contract for classes that
+     implements the interface.
+ --> An interface is similar to an abstract class, but it can be implemented by multiple classes
+ --> It can not have state ( we can not create an object of it)
+ --> Interfaces are custom type provided by kotlin that cannot be instantiated directly. Instead, these define a form
+     of behaviour that the implementing type have to follow( with the interface, you can define a set of
+     properties and methods, that the contract type must follow and implement)
+ --> one interface and class can implement multiple interfaces
+ */
+
+interface Perimeter{
+    val length:Int
+    fun findPerimeter()
+}
--- src/Polymorphism.kt
+++ src/Polymorphism.kt
@@ -10,26 +10,59 @@
  -> Helps in interacting with objects via common interface
 */
 
-open class Shape{
+open class Shape:Perimeter{
+    override val length: Int
+        get() = TODO("Not yet implemented")
     open fun area():Double = 0.0
+    override fun findPerimeter(){}
+
 }
 class Circle(private val radius: Int): Shape(){
+    override val length: Int
+        get() = TODO("Not yet implemented")
     override fun area():Double = Math.PI*radius*radius
+    override fun findPerimeter() {
+        println("The perimeter of Circle is "+2*Math.PI*radius)
+    }
 }
 class Square(private val side: Int): Shape(){
+    override val length: Int
+        get() = TODO("Not yet implemented")
     override fun area():Double = side*side+0.0
     override fun toString(): String {
         return "override by to string"
     }
+
+    override fun findPerimeter() {
+        println("The perimeter of Square is "+4*side)
+    }
 }
 class Triangle(private val side: Int): Shape(){
+    override val length: Int
+        get() = TODO("Not yet implemented")
     override fun area():Double = 0.5*side*side
+    override fun findPerimeter() {
+        println("The perimeter of Triangle is " + 3*side )
+    }
+}
+class Cylinder(val radius: Int, val hight: Int):Perimeter{
+    override val length: Int
+        get() = TODO("Not yet implemented")
+    override fun findPerimeter() {
+        println("The perimeter of cylinder is " + 4*radius+2*hight)
+    }
 }
 fun calculateAreaOfShapes(shapes:Array<Shape>){
     for(shape in shapes){
         println(shape.area())
     }
 }
+fun calculatePerimeter(obj:Array<Perimeter>){
+    for(objects in obj){
+        objects.findPerimeter()
+    }
+}
+
 
 fun main() {
     val shape1:Shape = Circle(5)
@@ -40,6 +73,10 @@ fun main() {
     // Created the array of shape type
     val shape =  arrayOf(Circle(5),Square(10),Triangle(20))
     calculateAreaOfShapes(shape)
+
+    // Creating the objects of classes which are implementing Interfaces
+    val  obj = arrayOf(Circle(10),Square(5),Triangle(10),Cylinder(5,10))
+    calculatePerimeter(obj)
 }
 /*
 -----------------Output---------------------------
@@ -48,4 +85,8 @@ fun main() {
 78.53981633974483
 100.0
 200.0
+The perimeter of Circle is 62.83185307179586
+The perimeter of Square is 20
+The perimeter of Triangle is 30
+The perimeter of cylinder is 2020
  */
